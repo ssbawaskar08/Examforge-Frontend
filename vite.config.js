f37c6bc1ced9a407c48d27bb6894b9761f669b19
Shreyas Bawaskar <shreyasbawaskar0812@gmail.com>
2026-05-27 10:51:31 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ export default defineConfig({
   server: {
     port: 5173,
     proxy: {
-      '/api': { target: 'https://examforge-backend.vercel.app/api', changeOrigin: true },
       '/socket.io': { target: 'http://localhost:5000', changeOrigin: true, ws: true },
     },
   },
